9f46a33c6869d82f675f22e94ed4963763b7e228
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-19 23:22:49 +0530

    this is third commit

---
+++ src/Ramp/Dips.java
@@ -0,0 +1,17 @@
+package Ramp;
+
+import java.util.Scanner;
+
+public class Dips {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+
+        System.out.println(a + b);
+
+    /*    Scanner scan =new Scanner(System.in);*/
+    }
+
+}
